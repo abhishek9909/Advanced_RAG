ec42e2e173abd835a3d181e348b895efe48d5e2d
Abhishek Mishra <mishra.abhishek199917@gmail.com>
2024-01-07 00:15:45 +0530

    better

--- server/completion.py
+++ server/completion.py
@@ -1,14 +1,14 @@
 import os
 from langchain_community.llms.ctransformers import CTransformers
 
-def post_completion(prompt, temperature = 0.0, context_len = 4000):
+async def post_completion(prompt, temperature = 0.0, context_len = 4000):
     model_id = "TheBloke/Mistral-7B-Instruct-v0.1-GGUF"
     os.environ['XDG_CACHE_HOME'] = '../assets/'
-    config = { 'temperature': temperature, 'context_length': context_len, "stream": False }
+    config = { 'temperature': temperature, 'context_length': context_len, "stream": True }
     llm = CTransformers(
         model = model_id,
         model_type = "mistral",
         config = config,
     )
-    print(llm.generate([prompt]))
- 
+    response = await llm.agenerate(prompts = [prompt])
+    print(response)
--- server/playground.py
+++ server/playground.py
@@ -2,5 +2,5 @@ from completion import post_completion
 import asyncio
 
 if __name__ == "__main__":
-    #asyncio.run(post_completion("Write a poem on America in 20 words or less"))
-    post_completion("Write a poem on America in 20 words or less")
+    asyncio.run(post_completion("Write a poem on America in 20 words or less"))
+    #post_completion("Write a poem on America in 20 words or less")
